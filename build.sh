fd81fdbb06a24b9090561fe8a57bc71d833a669b
muralidharan <muralidramachandran@gmail.com>
2024-10-19 01:21:23 +0530

    cleanup codebase

--- build.sh
+++ build.sh
@@ -4,61 +4,14 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
 # COPR repos
-curl -Lo /etc/yum.repos.d/_copr_robot-veracrypt-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/robot/veracrypt/repo/fedora-"${RELEASE}"/robot-veracrypt-fedora-"${RELEASE}".repo
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+/tmp/copr.sh
 
 # this installs a package from fedora repos
-# printer / scanner utils
-rpm-ostree install epson-inkjet-printer-escpr
-rpm-ostree install epson-inkjet-printer-escpr2
-rpm-ostree install printer-driver-brlaser
-rpm-ostree install simple-scan
-rpm-ostree install foo2zjs
-rpm-ostree install hplip
+/tmp/packages.sh
 
-# utilities
-rpm-ostree install p7zip
-rpm-ostree install p7zip-plugins
-rpm-ostree install wl-clipboard
-rpm-ostree install wireguard-tools
-rpm-ostree install veracrypt
+# systemd units
+/tmp/systemd-units.sh
 
-# DE tools
-rpm-ostree install gnome-shell-extension-system-monitor
-rpm-ostree install gnome-shell-extension-appindicator
-rpm-ostree install gnome-shell-extension-blur-my-shell
-rpm-ostree install ulauncher
-
-# Development
-rpm-ostree install code
-rpm-ostree install make
-rpm-ostree install python3-pip
-rpm-ostree install libxcrypt-compat
-rpm-ostree override remove toolbox --install distrobox
-
-# Terminal tools
-rpm-ostree install zoxide
-rpm-ostree install stow
-rpm-ostree install git
-rpm-ostree install zsh
-rpm-ostree install fastfetch
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
-
-# modifications to /etc/
-# ZRAM conf
-cp /usr/lib/systemd/zram-generator.conf /usr/lib/systemd/zram-generator.conf.bkp
-echo -e "\n# Default algorithm changed from lzo-rle to zstd \ncompression-algorithm = zstd" | tee -a /usr/lib/systemd/zram-generator.conf
-echo -e "# zram conf copied from PopOS\nvm.swappiness = 180\nvm.watermark_boost_factor = 0\nvm.watermark_scale_factor = 125\nvm.page-cluster = 0" | tee -a /etc/sysctl.d/99-vm-zram-parameters.conf
-
-# WiFi configuration
-echo -e "[connection]\nwifi.powersave=2\n" | tee -a /etc/NetworkManager/conf.d/wifi-powersave-off.conf
+# Customizations
+/tmp/customizations.sh
---
+++ build_files/packages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# printer / scanner utils
+rpm-ostree install epson-inkjet-printer-escpr
+rpm-ostree install epson-inkjet-printer-escpr2
+rpm-ostree install printer-driver-brlaser
+rpm-ostree install simple-scan
+rpm-ostree install foo2zjs
+rpm-ostree install hplip
+rpm-ostree install firewall-config
+
+# utilities
+rpm-ostree install p7zip
+rpm-ostree install p7zip-plugins
+rpm-ostree install wl-clipboard
+rpm-ostree install wireguard-tools
+rpm-ostree install veracrypt
+rpm-ostree install solaar
+rpm-ostree install git-credential-libsecret
+rpm-ostree install input-remapper
+rpm-ostree install lm_sensors
+rpm-ostree install rclone
+rpm-ostree install restic
+rpm-ostree install setools-console
+rpm-ostree install openssh-askpass
+
+# fonts
+rpm-ostree install opendyslexic-fonts
+
+# DE tools
+rpm-ostree install gnome-shell-extension-system-monitor
+rpm-ostree install gnome-shell-extension-appindicator
+rpm-ostree install gnome-shell-extension-blur-my-shell
+rpm-ostree install gnome-shell-extension-caffeine
+rpm-ostree install ulauncher
+
+# Development
+rpm-ostree install code
+rpm-ostree install make
+rpm-ostree install python3-pip
+rpm-ostree install libxcrypt-compat
+rpm-ostree override remove toolbox --install distrobox
+
+# Terminal tools
+rpm-ostree install zoxide
+rpm-ostree install stow
+rpm-ostree install git
+rpm-ostree install zsh
+rpm-ostree install fastfetch
+
+# Cleanup unnecessary apps
+rpm-ostree override remove gnome-extensions-app
+rpm-ostree override remove gnome-tour
+rpm-ostree override remove firefox-langpacks
+rpm-ostree override remove firefox
+rpm-ostree override remove podman-docker
+# rpm-ostree override remove ublue-os-update-services

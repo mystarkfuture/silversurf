34da28a2bf6d88f4e2706b1a64711b934bc75638
muralidharan <muralidramachandran@gmail.com>
2024-10-21 16:39:54 +0530

    updated file paths

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ rsync -rvK /tmp/ctx/fsroot/ /
 
 chmod +x /tmp/ctx/build_files/*
 
-/ctx/buikd_files/copr.sh
-/ctx/buikd_files/packages.sh
-/ctx/buikd_files/brew.sh
-/ctx/buikd_files/systemd.sh
-/ctx/buikd_files/mods.sh
+/tmp/ctx/build_files/copr.sh
+/tmp/ctx/build_files/packages.sh
+/tmp/ctx/build_files/brew.sh
+/tmp/ctx/build_files/systemd.sh
+/tmp/ctx/build_files/mods.sh
